19e5abbbf78a30c2b1e5877a2c3a019350c1ce81
Ijem <ij.ofili@gmail.com>
2024-09-26 18:47:29 +0100

    added entry
    added entry, see diffs.

--- creativecoding/scratchpad.js
+++ creativecoding/scratchpad.js
@@ -1,3 +1,8 @@
 let someVariable = 3;
 let nextVariableAfter_someVariable = someVariable + 1;
 console.log(nextVariableAfter_someVariable);
+
+let Multiplication_fn = (a, b) => {
+    return a * b;
+}
+console.log(Multiplication_fn(2, 5));
