6d13f13fce2b126983c1f38e0ba4a00a0fed3505
Ijem <ij.ofili@gmail.com>
2025-05-03 18:36:53 +0100

    added entry

--- creativecoding/scratchpad.js
+++ creativecoding/scratchpad.js
@@ -6,3 +6,18 @@ let Multiplication_fn = (a, b) => {
     return a * b;
 }
 console.log(Multiplication_fn(2, 5));
+
+let Multiplication_fn2 = (a, b) => a * b;
+console.log(Multiplication_fn2(2, 5));
+let Multiplication_fn3 = (a, b) => a * b;
+console.log(Multiplication_fn3(2, 5));
+let Multiplication_fn4 = (a, b) => a * b;
+console.log(Multiplication_fn4(2, 5));
+let Multiplication_fn5 = (a, b) => a * b;
+console.log(Multiplication_fn5(2, 5));
+let Multiplication_fn6 = (a, b) => a * b;
+console.log(Multiplication_fn6(2, 5));
+let Multiplication_fn7 = (a, b) => a * b;
+console.log(Multiplication_fn7(2, 5));
+
+
